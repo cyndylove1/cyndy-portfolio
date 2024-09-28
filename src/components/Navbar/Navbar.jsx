bb19e333341f1f29efb647d58ea314eaaa6a542f
cynthia <nwekecynthia2015@gmail.com>
2024-09-27 19:27:49 -0700

    intial commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -31,13 +31,13 @@ const Navbar = () => {
   return (
     <div className={header ? 'z-50 w-full text-white bg-black fixed top-0' : 'text-white bg-black'}>
       <div className='flex items-center justify-between w-full h-20 px-3 xl:px-[75px] md:px-10 z-50 bg-black fixed'>
-        <div className='text-xl font-extrabold text-white flex justify-center items-center'>
+        <div className='flex items-center justify-center text-xl font-extrabold text-white'>
           <span className='text-blue px-2 text-xl'>
             <SiConekta />
           </span>
           <h2>CYNDY.</h2>
         </div>
-        <nav className='hidden lg:flex'>
+        <nav className='lg:flex hidden'>
           <ul className='flex items-center font-bold cursor-pointer gap-[30px]'>
             <Link to="hero" spy={true} smooth={true} duration={500} className='hover:text-blue transition duration-500 ease-in-out'>Home</Link>
             <Link to="about" spy={true} smooth={true} duration={500} className='hover:text-blue transition duration-500 ease-in-out'>About</Link>
@@ -59,7 +59,7 @@ const Navbar = () => {
             <AiOutlineMenuUnfold size={25} onClick={handleClick} />
           )}
         </div>
-        <div className={`${menu ? 'translate-x-0' : '-translate-x-full'} lg:hidden flex flex-col absolute bg-white text-black left-0 top-20 pt-8 pb-4 gap-8 w-full h-fit items-center transition-transform duration-500 font-bold cursor-pointer`}>
+        <div className={`${menu ? 'translate-x-0' : '-translate-x-full'} lg:hidden flex flex-col absolute bg-white text-black left-0 top-20 pt-8 pb-4 gap-8 w-full h-[30rem] items-center transition-transform duration-500 font-bold cursor-pointer`}>
           <Link to="hero" spy={true} smooth={true} duration={500} className='hover:text-blue xl:mr-10 md:mr-4 transition duration-500 ease-in-out'>Home</Link>
           <Link to="about" spy={true} smooth={true} duration={500} className='hover:text-blue xl:mr-10 md:mr-4 transition duration-500 ease-in-out'>About</Link>
           <Link to="services" spy={true} smooth={true} duration={500} className='hover:text-blue xl:mr-10 md:mr-4 transition duration-500 ease-in-out'>Services</Link>
@@ -67,7 +67,7 @@ const Navbar = () => {
           <Link to="projects" spy={true} smooth={true} duration={500} className='hover:text-blue xl:mr-10 md:mr-4 transition duration-500 ease-in-out'>Projects</Link>
           <Link to="contact" spy={true} smooth={true} duration={500} className='hover:text-blue xl:mr-10 md:mr-4 transition duration-500 ease-in-out'>Contacts</Link>
           <Link to="contact">
-            <button className='border-blue transform-translate-y-14 hover:scale-110 px-6 py-2 text-black lg:text-white transition duration-500 ease-in-out bg-transparent border-2 rounded-md'>
+            <button className='border-blue transform-translate-y-14 hover:scale-110 lg:text-white px-6 py-2 text-black transition duration-500 ease-in-out bg-transparent border-2 rounded-md'>
               Let's Talk!
             </button>
           </Link>
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -5,24 +5,24 @@ import { TbApiApp } from "react-icons/tb";
 
 const Services = () => {
   return (
-    <div className=''>
-      <div className='bg-black h-fit pb-32'>
-        <div className='text-center text-blue text-4xl font-extrabold pt-20'>Services</div>
-        <div className='md:grid-cols-2 lg:grid-cols-3 grid-cols-1 grid xl:mx-20 md:mx-10 mx-4'>
-          <div className='shadow shadow-blue h-64 text-center rounded-md mt-10 transform-translate-y-14 group:hover:scale-110 md:mr-4 ' data-aos="fade-up" data-aos-delay="200">
-            <h2 className='text-blue text-5xl py-2 flex text-center justify-center'><BiCodeAlt /></h2>
+    <div className='h-fit'>
+      <div className='h-fit pb-32 bg-black'>
+        <div className='text-blue pt-20 text-4xl font-extrabold text-center'>Services</div>
+        <div className='md:grid-cols-2 lg:grid-cols-3 xl:mx-20 md:mx-10 grid grid-cols-1 mx-4'>
+          <div className='shadow-blue transform-translate-y-14 group:hover:scale-110 md:mr-4  h-64 mt-10 text-center rounded-md shadow' data-aos="fade-up" data-aos-delay="200">
+            <h2 className='text-blue flex justify-center py-2 text-5xl text-center'><BiCodeAlt /></h2>
             <h2 className='text-blue text-lg font-bold'>Web Development</h2>
-            <h5 className='text-white text-lg px-1'>With ample years of experience in areas of design, i develop and deliver beautiful  and highly scalable web designs.</h5>
+            <h5 className='px-1 text-lg text-white'>With ample years of experience in areas of design, i develop and deliver beautiful  and highly scalable web designs.</h5>
           </div>
-          <div className='shadow shadow-blue h-64 text-center rounded-md mt-10 transform-translate-y-14 group:hover:scale-110 hover:text-white hover:bg-purple md:mr-4' data-aos="fade-up" data-aos-delay="400">
-            <h2 className='text-blue text-5xl py-2 flex text-center justify-center'><TbApiApp /></h2>
+          <div className='shadow-blue transform-translate-y-14 group:hover:scale-110 hover:text-white hover:bg-purple md:mr-4 h-64 mt-10 text-center rounded-md shadow' data-aos="fade-up" data-aos-delay="400">
+            <h2 className='text-blue flex justify-center py-2 text-5xl text-center'><TbApiApp /></h2>
             <h2 className='text-blue text-lg font-bold'>Api Integration</h2>
-            <h5 className='text-white text-lg px-2'>connecting different software applications via their APIs. This allows these applications to interact and share data.</h5>
+            <h5 className='px-2 text-lg text-white'>connecting different software applications via their APIs. This allows these applications to interact and share data.</h5>
           </div>
-          <div className='shadow shadow-blue h-64 text-center rounded-md mt-10 transform-translate-x-14 hover:scale-110 transition duration-500 ease-in-out ' data-aos="fade-up" data-aos-delay="500">
-            <h2 className='text-blue text-5xl py-2 flex text-center justify-center'><FaSitemap /></h2>
+          <div className='shadow-blue transform-translate-x-14 hover:scale-110  h-64 mt-10 text-center transition duration-500 ease-in-out rounded-md shadow' data-aos="fade-up" data-aos-delay="500">
+            <h2 className='text-blue flex justify-center py-2 text-5xl text-center'><FaSitemap /></h2>
             <h2 className='text-blue text-lg font-bold'>Web Hosting</h2>
-            <h5 className='text-white text-lg px-2'>The virtual equivalent to renting space online to store your website's files and data,Internet hosting service for clients.</h5>
+            <h5 className='px-2 text-lg text-white'>The virtual equivalent to renting space online to store your website's files and data,Internet hosting service for clients.</h5>
           </div>
         </div>
       </div>
